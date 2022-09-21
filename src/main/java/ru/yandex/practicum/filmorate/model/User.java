8e7f31eea2d9c1924334a89769b3b75d4684eb7d
Лев Клименко <klimenko.lev@yandex.ru>
2022-09-21 07:58:04 +0300

    up

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,13 @@
+package java.ru.yandex.practicum.filmorate.model;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+
+public class User {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private Duration duration;
+}
