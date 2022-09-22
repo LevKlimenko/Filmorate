03cf2a4ff63728d7760b4fa7c575e2503b957656
Лев Клименко <klimenko.lev@yandex.ru>
2022-09-22 09:43:08 +0300

    Done

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,15 +3,13 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.PastOrPresent;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
 @Builder
 public class User {
+    @NotNull
     @Positive(message = "Значение ID должно быть больше 0")
     private Integer id;
     @Email(message = "Введите действительный E-mail")
