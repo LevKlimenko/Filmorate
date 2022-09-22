4bf856f8c702c6d9bbcbe87b9e8b338e7bf6c908
Лев Клименко <klimenko.lev@yandex.ru>
2022-09-22 12:54:14 +0300

    Update Validation

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -10,14 +10,11 @@ import java.time.LocalDate;
 @Data
 @Builder
 public class Film {
-    @Positive(message = "Значение ID должно быть больше 0")
     private int id;
     @NotBlank(message = "Название не может быть пустым или состоять только из пробелов")
     private String name;
     @Size(max = 200, message = "Длина описания больше 200 символов")
     private String description;
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @Min(value = 1895 - 12 - 28, message = "Дата должна быть больше 1895-12-28")
     private LocalDate releaseDate;
     @PositiveOrZero(message = "Продолжительность должна быть больше или равно 0")
     private int duration;
